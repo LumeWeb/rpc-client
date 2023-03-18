1d66b2452fcf7a51d6beacd9305785453a36f80d
Derrick Hammer <derrick@derrickhammer.com>
2023-03-18 12:15:29 -0400

    *update dist

--- dist/sharedRelay.d.ts
+++
@@ -1,5 +0,0 @@
-import RPC from "@lumeweb/rpc";
-import RpcNetwork from "./network.js";
-export declare function setupRelay(network: RpcNetwork): Promise<void>;
-export declare function getActiveRelay(): RPC;
-//# sourceMappingURL=sharedRelay.d.ts.map
