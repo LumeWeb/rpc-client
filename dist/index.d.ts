788591b227e91c7ecdfe32ef99865239386ed090
Derrick Hammer <derrick@derrickhammer.com>
2023-04-06 13:40:53 -0400

    *update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 export * from "./types.js";
 export * from "./query/index.js";
 export * from "./network.js";
+export * from "./util.js";
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
 export * from "./types.js";
 export * from "./query/index.js";
 export * from "./network.js";
+export * from "./util.js";
